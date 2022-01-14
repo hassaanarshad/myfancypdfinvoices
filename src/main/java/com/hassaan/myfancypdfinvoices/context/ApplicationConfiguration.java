530edd9683f29eeb0b06d899a965df9e2583977f
Hassaan Arshad <hassaanarshad65@gmail.com>
2022-01-14 13:53:03 +0500

    New stuff

--- src/main/java/com/hassaan/myfancypdfinvoices/context/ApplicationConfiguration.java
+++ src/main/java/com/hassaan/myfancypdfinvoices/context/ApplicationConfiguration.java
@@ -1,20 +1,31 @@
 package com.hassaan.myfancypdfinvoices.context;
 
+import javax.sql.DataSource;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hassaan.myfancypdfinvoices.ApplicationLauncher;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
 @PropertySource("classpath:/application.properties")
 @PropertySource(value = "classpath:/application-${spring.profiles.active}.properties", ignoreResourceNotFound = true)
 @EnableWebMvc
+@EnableTransactionManagement
 public class ApplicationConfiguration {
 
     // @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
@@ -27,6 +38,26 @@ public class ApplicationConfiguration {
     // }
 
     @Bean
+    public DataSource dataSource() {
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL("jdbc:h2:~/myFirstH2Database;INIT=RUNSCRIPT FROM 'classpath:schema.sql'");
+        ds.setUser("sa");
+        ds.setPassword("sa");
+        
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public TransactionManager platformTransactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
     }
@@ -34,5 +65,29 @@ public class ApplicationConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+
+        viewResolver.setOrder(1); // optional
+        viewResolver.setViewNames(new String[] {"*.html", "*.xhtml"}); // optional
+        return viewResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setCacheable(false);
+        return templateResolver;
     }
 }
--- src/main/java/com/hassaan/myfancypdfinvoices/service/InvoiceService.java
+++ src/main/java/com/hassaan/myfancypdfinvoices/service/InvoiceService.java
@@ -1,6 +1,9 @@
 package com.hassaan.myfancypdfinvoices.service;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.annotation.PostConstruct;
@@ -10,14 +13,20 @@ import com.hassaan.myfancypdfinvoices.model.Invoice;
 import com.hassaan.myfancypdfinvoices.model.User;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Component
 public class InvoiceService {
 
-    private List<Invoice> invoices = new CopyOnWriteArrayList<>();
+    private final JdbcTemplate jdbcTemplate;
 
     private final UserService userService;
+
     private final String cdnUrl;
 
     @PostConstruct
@@ -32,24 +41,62 @@ public class InvoiceService {
         // TODO actual deletion of PDFs
     }
 
-    public InvoiceService(UserService userService, @Value("${cdn.url}") String cdnUrl) {
+    public InvoiceService(UserService userService, JdbcTemplate jdbcTemplate, @Value("${cdn.url}") String cdnUrl) {
         this.userService = userService;
         this.cdnUrl = cdnUrl;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
+    @Transactional
     public List<Invoice> findAll() {
-        return invoices;
+        // return invoices;
+        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
+        return jdbcTemplate.query("select id, user_id, pdf_url, amount from invoices", (resultSet, rowNum) -> {
+            Invoice invoice = new Invoice();
+            invoice.setId(resultSet.getObject("id").toString());
+            invoice.setUserId(resultSet.getString("user_id"));
+            invoice.setPdfUrl(resultSet.getString("pdf_url"));
+            invoice.setAmount(resultSet.getInt("amount"));
+
+            return invoice;
+        });
     }
 
+    @Transactional
     public Invoice create(String userId, Integer amount) {
-        User user = userService.findById(userId);
-        if (user == null) {
-            throw new IllegalStateException();
-        }
+        // User user = userService.findById(userId);
+        // if (user == null) {
+        //     throw new IllegalStateException();
+        // }
 
-        Invoice invoice = new Invoice(userId, cdnUrl + "/images/default/sample.pdf", amount);
-        invoices.add(invoice);
+        // Invoice invoice = new Invoice(userId, cdnUrl + "/images/default/sample.pdf", amount);
+        // invoices.add(invoice);
 
+        // return invoice;
+        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
+        String generatedPdfUrl = cdnUrl + "/images/default/sample.pdf";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into invoices (user_id, pdf_url, amount) values (?, ?, ?)",
+            Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, userId);
+            preparedStatement.setString(2, generatedPdfUrl);
+            preparedStatement.setInt(3, amount);
+
+            return preparedStatement;
+        }, keyHolder);
+
+        String uuid = !keyHolder.getKeys().isEmpty() ? ((UUID) keyHolder.getKeys().values().iterator().next()).toString()
+        : null;
+
+        Invoice invoice = new Invoice();
+        invoice.setId(uuid);
+        invoice.setPdfUrl(generatedPdfUrl);
+        invoice.setAmount(amount);
+        invoice.setUserId(userId);
+        
         return invoice;
     }
 }
